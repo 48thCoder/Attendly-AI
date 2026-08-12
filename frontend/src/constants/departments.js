ef2453b9c634c425ed4a6159ea9950b53bff2660
48thCoder <guptavaibhav4248@gmail.com>
2026-08-12 20:41:29 +0530

    feat: add euclideanDistance utility and department constants

---
+++ frontend/src/constants/departments.js
@@ -0,0 +1,12 @@
+export const DEPARTMENTS = [
+  'CSE Core',
+  'CSE (AI)',
+  'CSE (AIML)',
+  'AIML',
+  'CSE (DS)',
+  'CS (IT)',
+  'CSE (IOT)',
+  'CSE (H)'
+];
+
+export const DEPARTMENTS_WITH_ALL = ['All', ...DEPARTMENTS];
